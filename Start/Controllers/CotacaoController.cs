3a048fbc9e449c06f71aab579ebbf76480a42a33
Renato Gomes <reegomescel@gmail.com>
2019-08-05 23:09:57 -0300

    Adicionando opção de valores de planos

--- Start/Controllers/CotacaoController.cs
+++ Start/Controllers/CotacaoController.cs
@@ -1,26 +1,11 @@
 ﻿using RestSharp;
 using RestSharp.Serialization.Json;
-using System.Collections.Generic;
 using System.Web.Http;
-
 
 namespace Start.Controllers
 {
     public class CotacaoController : ApiController
     {
-        // GET: api/Cotacao
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Cotacao/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-
 
         [HttpGet]
         public int GetSoma(int c, int d) => c + d;
@@ -36,24 +21,63 @@ namespace Start.Controllers
             IRestResponse restResponse = restClient.Execute(restRequest);
             Retorno dadosRetorno = new JsonDeserializer().Deserialize<Retorno>(restResponse);
 
-            /*
-            var response = restClient.Execute<List<Retorno>>(restRequest);
-            Retorno dadosRetorno = new JsonDeserializer().Deserialize<Retorno>(response);
+            //string fipeRetorno = string.Format("ID: {0}, Key: {1}, Fipe_Name: {2}, Name: {3}, Order: {4}, Preço: {5}", dadosRetorno.Id, dadosRetorno.Key, dadosRetorno.Fipe_Name, dadosRetorno.Name, dadosRetorno.Order, dadosRetorno.Preco);
 
-            foreach (Retorno item in response.Data)
-            {
-                string fipeRetorno = string.Format("ID: {0}, Key: {1}, Fipe_Name: {2}, Name: {3}, Order: {4}", dadosRetorno.Id, dadosRetorno.Key, dadosRetorno.Fipe_Name, dadosRetorno.Name, dadosRetorno.Order);
-                return fipeRetorno;
-            }
-            */
-
-            string fipeRetorno = string.Format("ID: {0}, Key: {1}, Fipe_Name: {2}, Name: {3}, Order: {4}, Preço: {5}", dadosRetorno.Id, dadosRetorno.Key, dadosRetorno.Fipe_Name, dadosRetorno.Name, dadosRetorno.Order, dadosRetorno.Preco);
-            return fipeRetorno;
+            string fipeValor = dadosRetorno.Preco.Remove(0, 3);
+            
+            return "Valor do plano: " + TabelaDeValores(float.Parse(fipeValor)).ToString();
         }
 
+        private const float plano1 = 589.70f;
+        private const float plano2 = 657.74f;
+        private const float plano3 = 940.79f;
+        private const float plano4 = 1059.36f;
+        private const float plano5 = 1160.16f;
+        private const float plano6 = 1298.42f;
+        private const float plano7 = 1440.73f;
+        private const float plano8 = 1612.37f;
+        private const float plano9 = 1805.72f;
 
+        private float TabelaDeValores(float valor)
+        {
+            float resultValor = 0;
 
+            if (valor <= 10000)
+                resultValor = plano1;
+            else if (valor >= 10001 && valor <= 20000)
+                resultValor = plano2;
+            else if (valor >= 20001 && valor <= 30000)
+                resultValor = plano3;
+            else if (valor >= 30001 && valor <= 40000)
+                resultValor = plano4;
+            else if (valor >= 40001 && valor <= 50000)
+                resultValor = plano5;
+            else if (valor >= 50001 && valor <= 60000)
+                resultValor = plano6;
+            else if (valor >= 60001 && valor <= 70000)
+                resultValor = plano7;
+            else if (valor >= 70001 && valor <= 80000)
+                resultValor = plano8;
+            else if (valor >= 80001 && valor <= 90000)
+                resultValor = plano9;
+            else if (valor >= 90001)
+                resultValor = 0;
 
+            return resultValor;
+        }
+
+        /*
+        // GET: api/Cotacao
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Cotacao/5
+        public string Get(int id)
+        {
+            return "value";
+        }
 
         // POST: api/Cotacao
         public void Post([FromBody]string value)
@@ -69,6 +93,7 @@ namespace Start.Controllers
         public void Delete(int id)
         {
         }
+        */
     }
     public class Retorno
     {
